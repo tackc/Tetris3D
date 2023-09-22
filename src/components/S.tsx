f8344ce3eb9371802896590ed7907716cac4f6ca
Chris Tack <chris_tack@mac.com>
2023-09-22 11:02:50 -0700

    Refactor shape

--- src/components/S.tsx
+++ src/components/S.tsx
@@ -1,13 +1,27 @@
+import { useEffect, useState } from "react";
 import { ITetrominoProps } from "../definitions";
 import Mino from "./Mino";
 
 const S = ({ position, groupRef }: ITetrominoProps) => {
+  const [matrix, setMatrix] = useState([
+    [0, 1, 1],
+    [1, 1, 0],
+    [0, 0, 0],
+  ]);
+
+  useEffect(() => console.log(groupRef), [groupRef]);
+
   return (
-    <group position={position} ref={groupRef}>
-      <Mino position={[0, 0, 0]} />
-      <Mino position={[1, 0, 0]} />
-      <Mino position={[0, 1, 0]} />
-      <Mino position={[-1, 1, 0]} />
+    <group position={[4.5 - position[0], 19.5 - position[1], 0]} ref={groupRef}>
+      {matrix.map((row, i) =>
+        row.map((column, j) => {
+          if (column === 1) {
+            return <Mino key={`${i}${j}`} position={[-j, -i, 0]} />;
+          } else {
+            return null;
+          }
+        })
+      )}
     </group>
   );
 };
